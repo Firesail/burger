ae1c95095ea530144f5ac7485a09d216a05cf535
Firesail <sailfire23@gmail.com>
2021-03-08 16:22:41 +0530

    Added Dynamic State

--- src/Components/Burger/Burger.js
+++ src/Components/Burger/Burger.js
@@ -4,12 +4,29 @@ import './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    
+    let transIngredients = Object.keys(props.ingredients).map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, i) => {
+            return <BurgerIngredient key={igKey + i} type={igKey} />
+        }); 
+    })
+    .reduce((arr, el) => {
+        return arr.concat(el)
+    }, []);
+
+    console.log(transIngredients.length);
+    if(transIngredients.length === 0){
+        transIngredients = <p>Please Start Adding :D</p>
+    }
+
+
     return(
         <div className="Burger">
             <BurgerIngredient type = "bread-top"/>
-            <BurgerIngredient type = "cheese"/>
+            {/* <BurgerIngredient type = "cheese"/>
             <BurgerIngredient type = "bacon"/>
-            <BurgerIngredient type = "cheese"/>
+            <BurgerIngredient type = "cheese"/> */}
+            {transIngredients}
             <BurgerIngredient type = "bread-bottom"/>
         </div>
     )
--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -3,11 +3,19 @@ import Auxy from '../../HOC/Auxy';
 import Burger from '../../Components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0
+        }
+    }
+
     render(){
         return(
             <Auxy>
                 <div>Burger</div>
-                <Burger/>
+                <Burger ingredients = {this.state.ingredients}/>
                 <div>Build Controls</div>
             </Auxy>
         );
